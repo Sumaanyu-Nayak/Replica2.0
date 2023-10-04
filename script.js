075101cb37fc384458e059a2068b9f5ff5e26f96
Binit <binitlenka@gmail.com>
2023-10-04 18:02:00 +0530

    Update some features

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const header = document.querySelector('.header');
-const hiddenbutton = document.querySelector('.TopButton');
-const btn = document.getElementById('goup');    
+const hiddenbutton = document.getElementById('TopButton')
 
-btn.addEventListener('click', () => {
-    // Scrolling to the top of a page
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-    })
-});
+function checkScroll() {
+    const scrollPosition = document.documentElement.scrollTop;
+
+    if(scrollPosition > (0.04*window.innerHeight)) {
+        hiddenbutton.classList.add('active');
+    } else {
+        hiddenbutton.classList.remove('active');
+    }
+}
+
+window.addEventListener('scroll', checkScroll);
